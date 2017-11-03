24d8ad8d3c89630332849e95183d02474c109f9d
Zachary Kucera <zkucera@uwo.ca>
2017-11-03 20:13:16 +0000

    Editing bears by ID

--- server.js
+++ server.js
@@ -60,7 +60,41 @@ router.route('/bears')
 
             res.json(bears);
         });
+    })
+    
+    
+    router.route('/bears/:bear_id')
+
+    // get the bear with that id (accessed at GET http://localhost:8080/api/bears/:bear_id)
+    .get(function(req, res) {
+        Bear.findById(req.params.bear_id, function(err, bear) {
+            if (err)
+                res.send(err);
+            res.json(bear);
+        });
+    })
+    
+    .put(function(req, res) {
+
+        // use our bear model to find the bear we want
+        Bear.findById(req.params.bear_id, function(err, bear) {
+
+            if (err)
+                res.send(err);
+
+            bear.name = req.body.name;  // update the bears info
+
+            // save the bear
+            bear.save(function(err) {
+                if (err)
+                    res.send(err);
+
+                res.json({ message: 'Bear updated!' });
+            });
+
+        });
     });
+
 
 
 
